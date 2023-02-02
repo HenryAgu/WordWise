3282889087e51ba1a9660442fc9d80ebc428550e
Henry <aguhenrychuks@gmail.com>
2023-02-02 23:37:18 +0100

    feat: landing page completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 // Components
-import Benefits from "../sub components/Benefits";
-import CustomerReview from "../sub components/CustomerReview";
-import Feature from "../sub components/Feature";
-import Hero from "../sub components/Hero";
-import HowItWorks from "../sub components/HowItWorks";
+import Benefits from "../home components/Benefits";
+import CustomerReview from "../home components/CustomerReview";
+import Feature from "../home components/Feature";
+import Hero from "../home components/Hero";
+import HowItWorks from "../home components/HowItWorks";
 
 // stylesheet
 import "./Home.css";
